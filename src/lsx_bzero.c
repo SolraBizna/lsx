bebd8b07bb049916a7f5bedf1a0f22a5495aef0e
Solra Bizna <solra@bizna.name>
2015-11-17 13:39:34 -0700

    Paranoia extremis

--- src/lsx_bzero.c
+++ src/lsx_bzero.c
@@ -2,4 +2,11 @@
 extern void* memset(void* s, int c, size_t n);
 void lsx_explicit_bzero(void* p, size_t n) {
   memset(p, 0, n);
+  // In case we were compiled with optimizations... borrow a trick from
+  // BoringSSL
+#if __GNUC__
+  __asm__ volatile("" : : "r"(s) : "memory");
+#elif _MSC_VER
+  asm;
+#endif
 }
