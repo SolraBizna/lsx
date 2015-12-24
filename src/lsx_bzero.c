fe4b2ebb58b259f87f680177bc6d3cd4084b89f0
Solra Bizna <solra@bizna.name>
2015-12-24 01:15:25 -0700

    allow to compile as C++

--- src/lsx_bzero.c
+++ src/lsx_bzero.c
@@ -1,7 +1,15 @@
 #include <stddef.h>
 
 // Compile this file without optimizations, and, if needed, with -fno-builtins
-extern void* memset(void* s, int c, size_t n);
+extern
+#if __cplusplus
+"C"
+#endif
+void* memset(void* s, int c, size_t n);
+
+#if __cplusplus
+extern "C"
+#endif
 void lsx_explicit_bzero(void* p, size_t n) {
   memset(p, 0, n);
   // In case we were compiled with optimizations... borrow a trick from
